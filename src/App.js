4d70fe75f8e52d5a100b3d00aee427d090a4699a
TreshMiralissa <muratet.jade@yahoo.com>
2022-11-18 09:32:16 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import Title from './components/title.js';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Title/>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React @ BeCode
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/title.js
@@ -0,0 +1,3 @@
+const Title = () => <h1>My Todo modified App</h1>
+
+export default Title;
