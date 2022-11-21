3bacdcd03c6a5e99c15fca8431371cd4b3f058e9
TreshMiralissa <muratet.jade@yahoo.com>
2022-11-21 13:30:33 +0100

    todoList state and key

--- src/components/todoList.js
+++ src/components/todoList.js
@@ -1,11 +1,34 @@
-// import React, { useState } from "react";
+// import React from "react";
+import React, { useState } from "react";
 
-export default function TodoList(){
-    const initialTodos = ["Translate informations", "Learn React", "Be Awesome !"];;
-    
-    const listItems = initialTodos.map((todo)=>
-    <li key={todo.id()}>{todo}</li>);
-    return(
-        <ul>{listItems}</ul>
-    );
+
+export default function TodoList() {
+  let idUnique = 0;
+  const initialTodos = ["Translate informations", "Learn React", "Be Awesome !"];
+  const [todos, setTodos] = useState(initialTodos);
+  const [checked, setChecked] = React.useState(false);
+
+  const handleChangeset = event => {
+    if (event.target.checked) {
+      console.log('✅ Checkbox is checked');
+    } else {
+      console.log('⛔️ Checkbox is NOT checked');
+    }
+    setChecked(current => !current);
+  };
+
+  return (
+    <ul>
+      {todos.map((todo) => (
+        <li key={idUnique++}>
+          <input type="checkbox"
+          defaultChecked={checked}
+          onChange={handleChangeset}          
+          />{todo};
+        </li>
+      
+      ))}
+    </ul>
+  );
 }
+
